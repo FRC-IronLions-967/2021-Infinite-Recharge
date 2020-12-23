5ab27c90ec8b4a9c9a6883cec0b84aca246e35d5
nastark123 <>
2020-12-23 10:26:51 -0600

    Update PathCreator.java

--- src/main/java/frc/robot/utils/navigation/PathCreator.java
+++ src/main/java/frc/robot/utils/navigation/PathCreator.java
@@ -1,12 +1,23 @@
 package frc.robot.utils.navigation;
 
+/*
+    This class will eventually
+*/
+
 public class PathCreator {
     private Node curNode;
     private Node prevNode;
 
+    private Node[] samplePath;
+    private int nodeIndex = 0;
+
     public PathCreator() {
-        curNode = new Node(0.0, 0.0, NodeType.NAVIGATION);
+        samplePath = new Node[] {new Node(0.0, 0.0, NodeType.NAVIGATION), new Node(3.0, 4.0, NodeType.NAVIGATION), new Node(3.0, 8.0, NodeType.NAVIGATION), new Node(0.0, 12.0, NodeType.NAVIGATION)};
+
+        curNode = samplePath[0];
     }
 
-    
+    public Node getNext() {
+        return samplePath[nodeIndex++];
+    }   
 }
