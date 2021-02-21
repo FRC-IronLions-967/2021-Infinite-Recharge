052aa8fb3f91dc74d2dfa2a4139b798e815e6467
nastark123 <43659552+nastark123@users.noreply.github.com>
2021-02-21 17:33:41 -0600

    Rewrote some vision code for better processign

--- Custom-Vision/src/main/java/TargetPipeline.java
+++ Custom-Vision/src/main/java/TargetPipeline.java
@@ -16,8 +16,8 @@ public class TargetPipeline implements VisionPipeline {
   public volatile double height;
   public volatile double width;
   public volatile boolean hasTarget = false;
-  public final double MIN_AREA_PROP = 0.12;
-  public final double MAX_AREA_PROP = 0.27;
+  public final double MIN_AREA_PROP = 0.08;
+  public final double MAX_AREA_PROP = 0.35;
 
   public volatile Mat result;
 
@@ -30,7 +30,7 @@ public class TargetPipeline implements VisionPipeline {
 
     Imgproc.blur(mat, mat, new Size(3, 3));
 
-    Imgproc.threshold(mat, mat, 230, 255, 0);
+    Imgproc.threshold(mat, mat, 240, 255, 0);
 
     Imgproc.Canny(mat, mat, 50, 200, 3, false);
 
@@ -50,19 +50,27 @@ public class TargetPipeline implements VisionPipeline {
       boundRects[i] = Imgproc.boundingRect(contoursPoly[i]);
     }
 
-    Rect bRect = new Rect();
-
     if (boundRects.length > 0) {
-      int maxIndex = 0;
+  
+      ArrayList<Rect> targets = new ArrayList<>();
       for (int i = 0; i < boundRects.length; i++) {
-          maxIndex = (boundRects[i].area() > boundRects[maxIndex].area()) ? i : maxIndex;
+          contourArea = Imgproc.contourArea(contours.get(i));
+          area = boundRects[i].area();
+          if(contourArea / area < MAX_AREA_PROP && contourArea / area > MIN_AREA_PROP) {
+            targets.add(boundRects[i]);
+          }
       }
-      contourArea = Imgproc.contourArea(contours.get(maxIndex));
-      bRect = boundRects[maxIndex];
-      area = bRect.area();
 
-      if(contourArea / area < MAX_AREA_PROP && contourArea / area > MIN_AREA_PROP) {
-        Imgproc.rectangle(mat, boundRects[maxIndex].tl(), boundRects[maxIndex].br(), new Scalar(0, 0, 255));
+      if(targets.size() > 0) {
+
+        int maxIndex = 0;
+
+        for(int i = 1; i < targets.size(); i++) {
+          maxIndex = (targets.get(i).area() > targets.get(maxIndex).area()) ? i : maxIndex;
+        }
+
+        Rect bRect = boundRects[maxIndex];
+        Imgproc.rectangle(mat, bRect.tl(), bRect.br(), new Scalar(0, 0, 255));
         tx = ((double)bRect.tl().x - (((double)mat.cols() / 2.0) - (double)bRect.width / 2.0));
         ty = ((((double)mat.rows() / 2.0) - (double)bRect.height / 2.0) - (double)bRect.tl().y);
         height = (double) bRect.height;
